7fb7a6bcb07fbe1daa0d05a8374ad63fff812247
[Grant Willis] <[thegwillis.gw@gmail.com]>
2023-04-21 12:28:14 -0700

    Schema and Seeds files functional

---
+++ db/seeds.sql
@@ -0,0 +1,18 @@
+INSERT INTO  department (department_name)
+VALUES ("Sales"),
+       ("Engineering"),
+       ("Legal");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ('CEO', 250000.00, 1),
+       ('COO', 200000.00, 1),
+       ('CFO', 180000.00, 2),
+       ('HRM', 150000.00, 2),
+       ('CRDO', 150000.00, 3),
+       ('Salesman', 100000.00, 2);
+       
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Maleficent","Bystander", 1, NULL),
+       ("Jafar", "Ugleman", 1, 2),
+       ("Taka", "Scar", 2, 3);
